ad501f6d44f33ff165f7873bedf1deeb1f40570f
Elec332 <arnout1998@gmail.com>
2015-04-06 21:40:58 +0200

    More work on multiblock stuff

--- src/main/java/elec332/eflux/tileentity/BaseMultiBlockMachine.java
+++ src/main/java/elec332/eflux/tileentity/BaseMultiBlockMachine.java
@@ -1,11 +1,9 @@
 package elec332.eflux.tileentity;
 
-import elec332.eflux.util.IEFluxTile;
 import net.minecraft.block.Block;
-import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraftforge.common.util.ForgeDirection;
 
 /**
  * Created by Elec332 on 5-4-2015.
@@ -14,15 +12,31 @@ public abstract class BaseMultiBlockMachine extends BaseMachineTEWithInventory{
 
     public BaseMultiBlockMachine(int maxEnergy, int maxReceive, int invSize) {
         super(maxEnergy, maxReceive, invSize);
-        if (this.formed)
-            tryToForm();
-        else this.formed = false;
     }
 
-    private boolean formed;
+    private boolean formed = false;
 
     public boolean isFormed() {
-        return formed;
+        return this.formed;
+    }
+
+    @Override
+    public void updateEntity() {
+        super.updateEntity();
+        if (timeCheck()) {
+            if (this.formed) {
+                if (!areBlocksAtRightLocations()) {
+                    this.formed = false;
+                    this.modifyEnergyStored(-this.getMaxEnergyStored(ForgeDirection.UNKNOWN));
+                    this.markDirty();
+                }
+            } else this.modifyEnergyStored(-this.getMaxEnergyStored(ForgeDirection.UNKNOWN));
+        }
+    }
+
+    @Override
+    public boolean onBlockActivated(EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
+        return this.isFormed() && super.onBlockActivated(player, side, hitX, hitY, hitZ);
     }
 
     @Override
@@ -38,10 +52,19 @@ public abstract class BaseMultiBlockMachine extends BaseMachineTEWithInventory{
     }
 
     public boolean tryToForm(){
-        if (areBlocksAtRightLocations())
+        if (areBlocksAtRightLocations()) {
             this.formed = true;
+            this.markDirty();
+            this.onCreated();
+        }
         return areBlocksAtRightLocations();
     }
 
     protected abstract boolean areBlocksAtRightLocations();
+
+    protected abstract void onCreated();
+
+    public Block getBlockAtOffset(int x, int y, int z){
+        return worldObj.getBlock(xCoord+x, yCoord+y, zCoord+z);
+    }
 }
--- src/main/java/elec332/eflux/tileentity/TEGrinder.java
+++ src/main/java/elec332/eflux/tileentity/TEGrinder.java
@@ -4,12 +4,14 @@ import elec332.eflux.client.inventory.GuiInventoryGrinder;
 import elec332.eflux.inventory.TEGrinderContainer;
 import elec332.eflux.util.Directions;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.init.Blocks;
 import net.minecraftforge.common.util.ForgeDirection;
+
 
 /**
  * Created by Elec332 on 3-4-2015.
  */
-public class TEGrinder extends BaseMachineTEWithInventory{
+public class TEGrinder extends BaseMultiBlockMachine{
 
     public TEGrinder() {
         super(60000, 40, 18);
@@ -22,12 +24,14 @@ public class TEGrinder extends BaseMachineTEWithInventory{
 
     @Override
     public boolean canConnectEnergy(ForgeDirection from) {
-        return Directions.getDirectionFromNumber(worldObj.getBlockMetadata(xCoord, yCoord, zCoord)) == from;
+        return Directions.getDirectionFromNumber(worldObj.getBlockMetadata(xCoord, yCoord, zCoord)) == from && this.isFormed();
     }
 
     @Override
     public boolean onBlockActivated(EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
-        return openGui(player);
+        if (!isFormed())
+            return tryToForm();
+        else return openGui(player);
     }
 
     @Override
@@ -38,5 +42,14 @@ public class TEGrinder extends BaseMachineTEWithInventory{
     @Override
     public Object getGuiClient(EntityPlayer player) {
         return new GuiInventoryGrinder(this, player);
+    }
+
+    @Override
+    protected boolean areBlocksAtRightLocations() {
+        return getBlockAtOffset(-1, 0, -1) == Blocks.beacon && getBlockAtOffset(-1, 0, 1) == Blocks.beacon && getBlockAtOffset(1, 0, 1) == Blocks.beacon && getBlockAtOffset(1, 0, -1) == Blocks.beacon;
+    }
+
+    @Override
+    protected void onCreated() {
     }
 }
